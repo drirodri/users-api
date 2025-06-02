84152738001b29e797556bfba77274f7a25772ad
drirodri <adriano.rsilva97@gmail.com>
2025-06-02 13:28:40 -0300

    feat: added entity first setup

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+export class User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(id: number, name: string, email: string, password: string) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+}
